ff0972c4aa5253cb7168f154f7b5c3064912150e
AndreyIsakov <deflorator1980yandex.ru>
2021-06-18 15:58:00 +0300

    percents

--- prediction_standardized.py
+++ prediction_standardized.py
@@ -1,19 +1,37 @@
 import cv2
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 import os
+
 
 folder = '/home/a/Documents/recognition_of_my_handwritten_digits/img_st/'
 files = [f for f in os.listdir(folder) if 'png' in f or 'jpg' in f]
 
+
 # model = load_model('straight_bs200e30.h5')
 model = load_model('es.h5')
 
+
 def predict(folder, file):
-    img = cv2.imread(folder+file, cv2.IMREAD_GRAYSCALE) 
-    im = img.reshape(1,28,28,1)/255
+    img = cv2.imread(folder+file, cv2.IMREAD_GRAYSCALE)
+    im = img.reshape(1, 28, 28, 1)/255
     return np.argmax(model.predict(im))
+
+
+count = 0
+for i in range(0, len(files)):
+    if int(files[i][0]) == predict(folder, files[i]):
+        count += 1
+
+
+print(count / len(files) * 100, '%')
+
 
 for f in files:
     print(f, predict(folder, f), sep='  ')
+
+
+count = 0
+for i in range(0, len(files)):
+    if int(files[i][0]) == predict(folder, files[i]):
+        count += 1
